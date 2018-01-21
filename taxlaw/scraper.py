99836c27fd269b01bde0f657a415a5f7a647579e
RONDELL KING <rking1@stevens.edu>
2018-01-21 00:10:36 -0500

    Minor updates

--- taxlaw/scraper.py
+++ taxlaw/scraper.py
@@ -1,27 +1,33 @@
-import urllib2
+import urllib.request as urllib2
 import csv
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+#quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+quote_page = 'http://rechart1.acgov.org/docdetail.asp?id=jAxNQ%3D%3DjV%03%13M%215b1%21k%217DAwMR%21%21tzI2O&ms=0&cabinet=opr&pg=&id2=TIvNwr%21E%21%03%0FMHqmbIqRYDE3%215FUBi8yM'
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, "html.parser") # Contains html
 
-name = soup.find("h1", attrs={"class": "name"}).text
-price = soup.find("div", attrs={"class": "price"}).text
+soup.get()
+td_tags = soup.find_all('td')
+td_tags = list(td_tags)
 
-data = []
-data.append((name,price))
+#Loop through
+tag = td_tags[39]
+tag.get_text('href')
+tag.get()
 
-print name
-print price
 
 
-def save_to_excel(name, data):
-    with open(name,'a') as csv_file:
-        writer = csv.writer(csv_file)
-        # The for loop
-        for name, price in data:
-            writer.row([name, price])
+print(td_tags)
+
+#print(soup.prettify())
+
+
+# name = soup.find("h1", attrs={"class": "name"}).text
+# price = soup.find("div", attrs={"class": "price"}).text
+#
+# data = []
+# data.append((name,price))
 
