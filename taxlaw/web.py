fee75a7e528e2cd4c8de3e6f84c6f2f2c7fac6b2
RONDELL KING <rking1@stevens.edu>
2018-11-15 23:50:42 -0500

    Baseline stuff

--- taxlaw/web.py
+++ taxlaw/web.py
@@ -1,31 +1,78 @@
 #! python3
 import pyperclip
+from taxlaw import Configuration
 import webbrowser
 import sys
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
-#browrser = webdriver.Firefox()
-#browser.get(url)
-#linkElem = browser.find_element_by_link_text('Link text')
-#linkElem.click() # Click link
-url = pyperclip.paste() # Takes information from clip board
-# webrowser.open() # If you need to open pages
-print(url)
-
-res= requests.get(url)
-res.raise_for_status()
-
-soup = BeautifulSoup(url)
-#soup.select(#)
+import datetime
+from dateutil import parser
 
 
 def getsite():
     pass
 
+
 def getlead():
     pass
 
+
 def write_results():
     pass
+
+
+def get_input_date(msg):
+    while True:
+        date = input(msg)
+        if date == 'end':
+            sys.exit('Closing web scaper')
+        try:
+            parser.parse(date)
+        except Exception:
+            print("Incorrect data format, should be MM-DD-YYYY")
+            continue
+        else:
+            return date
+
+
+def get_input_county(msg):
+    while True:
+        county = input(msg)
+        if county.lower() == 'end':
+            sys.exit('Closing web scaper')
+        try:
+            url = Configuration.loaded_sites[county.lower()]
+        except KeyError:
+            print("County {} not configured, try again (or type end)".format(url))
+            continue
+        else:
+            return url
+
+
+#browrser = webdriver.Firefox()
+#browser.get(url)
+#linkElem = browser.find_element_by_link_text('Link text')
+#linkElem.click() # Click link
+
+def main():
+
+    url = get_input_county('Enter the county name: ')
+    date = get_input_date('Enter the starting date: ')
+    #webbrowser.open(url)  # If you need to open pages
+
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = BeautifulSoup(res.content, "html.parser")
+    action_url = soup.find('form').get('action')
+    data = {"DOC_TYPE": "TAX LIENS (FED)",
+            "doc_date_A": "08/01/18",
+            "doc_date_B": "11/01/18"
+            }
+
+    url += url + action_url
+    requests.post(url, data=data)
+    #print(soup)
+if __name__ == '__main__':
+    main()
