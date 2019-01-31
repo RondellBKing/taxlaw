ba69c9b123ec9aa0530b4a181c9e3ba72b5730ab
mitchellahall <mitchellanthonyhall@gmail.com>
2019-01-31 07:34:09 -0700

    Starting to fix Marin inheritance

--- taxlaw/marin.py
+++ taxlaw/marin.py
@@ -7,29 +7,23 @@ class Marin(Scraper):
     url = 'https://apps.marincounty.org/RecordersIndexSearch?XHideDisclaimer=True' # after accepting disclaimer
     base_url = 'https://apps.marincounty.org'
 
-    def urls(url, base_url):
-        self.url = url
-        self.base_url = base_url
-
-    sdate = input('month/day/year: ')
-    edate = input('month/day/year: ')
-
-    smonth = sdate[0:2]
-    sday = sdate[3:5]
-    syear = sdate[6:]
-    emonth = edate[0:2]
-    eday = edate[3:5]
-    eyear = edate[6:]
-    result_url = 'https://apps.marincounty.org/RecordersIndexSearch/?Action=T&TDT=TAX%20LIEN&TED={}%2F{}%2F{}&TSD={}%2F{}%2F{}'.format(emonth, eday, eyear, smonth, sday, syear)
-
+    def make_url(self):
+        smonth = start_date[0:2]
+        sday = start_date[3:5]
+        syear = start_date[6:]
+        emonth = end_date[0:2]
+        eday = end_date[3:5]
+        eyear = end_date[6:]
+        result_url = 'https://apps.marincounty.org/RecordersIndexSearch/?Action=T&TDT=TAX%20LIEN&TED={}%2F{}%2F{}&TSD={}%2F{}%2F{}'.format(emonth, eday, eyear, smonth, sday, syear)
+        print(result_url)
 
 # table = bs.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="i0")
 # rows = table.findAll(lambda tag: tag.name=='tr')
 
-    res = requests.get(result_url)
-    res.raise_for_status()
-
-    soup = BeautifulSoup(res.content, "html.parser")
+    # res = requests.get(result_url)
+    # res.raise_for_status()
+    #
+    # soup = BeautifulSoup(res.content, "html.parser")
 # action_url = soup.find('form', id="T").get('action')
 # data = {"TDT" : "Tax Lien", # doc title
 #         "TSD" : "08012018",
