95a152afdeb37097cad2c24daecd15b737c7e6da
mitchellahall <mitchellanthonyhall@gmail.com>
2019-02-09 06:46:43 -0700

    marin now provides list of urls to targeted page results of TAX LIEN only

--- taxlaw/marin.py
+++ taxlaw/marin.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from scraper import Scraper
+from lxml import html
+import webbrowser
 
 
 class Marin(Scraper):
@@ -23,9 +25,50 @@ class Marin(Scraper):
         soup = BeautifulSoup(res.content, "html.parser")
         table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="i0")
         rows = table.findAll(lambda tag: tag.name=='tr')
-        for row in rows[1:]:
-            print(str(row) + '\n\n')
 
+        for tr in list(table):
+            if '>TAX LIEN<' in str(tr):
+                url = tr.a.get('href')
+                disclaimer = '&XHideDisclaimer=True'
+                page = url + disclaimer
+                print(page)
+#
+#         for element, tag, url, other in links_list[5:]:
+#             disclaimer = '&XHideDisclaimer=True'
+#             page = url + disclaimer
+#             print('\n' + page + '\n')
+#             # res = requests.get(page)
+#             # res.raise_for_status()
+#             # soup = BeautifulSoup(res.content, 'html.parser')
+#             # doc_title = '//*[@id="main-content"]/div[2]/div[3]/div/div[1]/table/tbody/tr[3]/td[2]'
+#             # if 'RELEASE' in soup:
+#             #     print('no result')
+#             # else:
+#             #     webbrowser.open(page)
+#
+# html = html.fromstring(str(rows[2]))
+# link = list(html.iterlinks())
+# link_as_list = str(link[0]).split(',')
+# url = actually_a_list[2][2:-1]
+#
 marin = Marin(input('Start Date: '), input('End Date: '))
 marin.make_url()
 marin.html_parse()
+#
+# for tr in table:
+#     if '>TAX LIEN<' in str(item):
+#         print(item)
+#
+# for item in list(rows):
+#     if '>TAX LIEN<' in str(item):
+#         html = html.fromstring(str(item))
+#         link = list(html.iterlinks())
+#         link_as_list = str(link[0]).split(',')
+#         url = actually_a_list[2][2:-1]
+#         print(url)
+#         # html_list = html.fromstring(str(item))
+#         # link = list(html_list.iterlinks())
+#         # print(link[2]) # link is a list of 1 item?
+#         # # print('\n' + str(item) + '\n')
+#     else:
+#         print('Wrong doc title')
