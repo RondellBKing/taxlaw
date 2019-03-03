a51905fb07d6759e76f6b560feaf8c05067408ed
mitchellahall <mitchellanthonyhall@gmail.com>
2019-03-03 04:55:17 -0700

    requirements and setup added

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
    author_email='mitchellanthonyhall@gmail.com',
    url='https://github.com/RondellBKing/taxlaw',
    packages=find_packages(),
-   install_requires=['', ''], #external packages as dependencies
+   install_requires=[
+   'beautifulsoup4 == 4.7.1',
+   'flask == 1.0.2',
+   'lxml == 4.3.1',
+   'pygsheets == 2.0.0'
+   ],
    python_requires='>=3.6.6'
 )
