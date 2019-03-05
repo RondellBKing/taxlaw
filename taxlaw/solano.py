5752d5c2024c6e1e965f782614014c6ca97b7974
mitchellahall <mitchellanthonyhall@gmail.com>
2019-03-05 14:27:10 -0700

    scrapes solano and returns target results unformated.  imports data.

--- taxlaw/solano.py
+++ taxlaw/solano.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from scraper import Scraper
 from lxml import html
+from solano_data import url, cookies, headers, data
+from datetime import datetime
 
 
 class Solano(Scraper):
@@ -10,42 +12,14 @@ class Solano(Scraper):
         super().__init__(start_date, end_date)
 
     def scrape(self):
-        self.url = 'http://recorderonline.solanocounty.com/Search/Pages/SearchResults.aspx'
-        res = requests.get(self.url)
-        res.raise_for_status()
+        page = requests.post(url, headers=headers, cookies=cookies, data=data)
+        soup = BeautifulSoup(page.content, "html.parser")
+        table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == "searchres_grid")
 
-        main_soup = BeautifulSoup(res.content, "html.parser")
-        action_url = main_soup.find('form').get('action')
-        print(action_url)
-        data = {
-            'ctl00$PlaceHolderMain$ucSearchInput$txtDocumentDateFrom': '1/1/2019',
-            'ctl00$PlaceHolderMain$ucSearchInput$txtDocumentDateTo': '2/28/2019',
-            'ctl00$PlaceHolderMain$ucSearchInput$drpFilingCode': '23',
-            'ctl00$PlaceHolderMain$ucSearchInput$hdnIsAdvanceSearchResult': 'Advance',
-            'ctl00$PlaceHolderMain$ucSearchResults$drpResultsPerPage': '100'
-            }
+        print(table)
 
-
-import webbrowser
-import requests
-from bs4 import BeautifulSoup
-
-url = 'http://recorderonline.solanocounty.com/Search/Pages/SearchResults.aspx'
-
-data = {
-    'ctl00$PlaceHolderMain$ucSearchInput$txtDocumentDateFrom': '1/1/2019',
-    'ctl00$PlaceHolderMain$ucSearchInput$txtDocumentDateTo': '2/28/2019',
-    'ctl00$PlaceHolderMain$ucSearchInput$drpFilingCode': '23',
-    'ctl00$PlaceHolderMain$ucSearchInput$hdnIsAdvanceSearchResult': 'Advance',
-    'ctl00$PlaceHolderMain$ucSearchResults$drpResultsPerPage': '100'
-    }
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint()
-
-requests.Session().get(url)
-requests.Session().post(url, data)
-page = requests.Session().get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == "searchres_grid")
+if __name__ == '__main__':
+    start_dt = datetime.strptime(input('Start Date(dd/mm/yyyy): '), "%d/%m/%Y")
+    end_dt = datetime.strptime(input('End Date(dd/mm/yyyy): '), "%d/%m/%Y")
+    solano = Solano(start_dt, end_dt)
+    solano.scrape()
