a7420ab643b1eb1452650c7438319a5d71b63181
mitchellahall <mitchellanthonyhall@gmail.com>
2019-04-03 10:00:17 -0600

    marin spider assemles url but url is not allowed

--- backend/scraper/scraper/spiders/marin.py
+++ backend/scraper/scraper/spiders/marin.py
@@ -1,15 +1,28 @@
 import scrapy
 from scraper.scraper.items import LienItem
+import urllib.parse
 
 class MarinSpider(scrapy.Spider):
     name = 'marin'
 
     def start_requests(self):
+       base_url = 'https://apps.marincounty.org/RecordersIndexSearch/%s'
+               
+       query = urllib.parse.urlencode({
+               'TDT': 'AFFIDAVIT',
+               'TSD': '04/01/2019',
+               'TED': '04/03/2019',
+               'Action': 'T',
+               })
+
+       action_url = base_url % query
+       
        urls = [
-               'https://apps.marincounty.org/RecordersIndexSearch/',
+               action_url,
                ]
+
        for url in urls:
-           yield scrapy.Request(url=url, callback=self.parse)
+           yield scrapy.Request(url=action_url, callback=self.parse)
 
     def parse(self, response):
         self.log('stuff happened')
